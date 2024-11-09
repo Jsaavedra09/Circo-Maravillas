cae3a63624deb609a01a97023faa0c37d6f1833e
ilemarie <ilemarie@uade.edu.ar>
2024-11-09 15:12:11 -0300

    NAV cn hamb
    lol xd

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-/* Escriba aca nachoo*/
+const sidebar = document.querySelector(".sidebar")
+const sidebar = document.querySelector(".sidebar") 
+
+    function showSidebar(){
+        sidebar.style.display = "flex"
+     }
+     function hideSidebar(){
+        sidebar.style.display = "none"
+    }
